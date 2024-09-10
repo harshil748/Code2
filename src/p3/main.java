b67016f6ccbd4ba0b967eb926461c291cc63b4fb
Harshil Patel <pharshil748@gmail.com>
2024-09-10 10:03:46 +0530

    code changes

--- src/p3/main.java
+++ src/p3/main.java
@@ -1,11 +1,28 @@
 package p3;
 
-class C3{
-    public void m3(){
-        System.out.println("Method m3 of class c3");
+class JoinEx extends Thread {
+    public static void main(String[] args) {
+        JoinEx t1 = new JoinEx();
+        JoinEx t2 = new JoinEx();
+        JoinEx t3 = new JoinEx();
+        t1.start();
+        try {
+            t1.join(); // join(milliseconds)
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        t2.start();
+        t3.start();
     }
-    public static void main(String[] args){
-        C3 obj = new C3();
-        obj.m3();
+
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            try {
+                Thread.sleep(500);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            System.out.println(i);
+        }
     }
 }
